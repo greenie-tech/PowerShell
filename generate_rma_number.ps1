18ff0a7de16ceadd26483b7ef0d62485deb935ff
greenie-tech <39019453+greenie-tech@users.noreply.github.com>
2018-05-09 01:21:08 -0400

    Add files via upload

---
+++ generate_rma_number.ps1
@@ -0,0 +1,16 @@
+﻿# genenates a RMA nmuber with a date stamp
+
+param (
+[int]$NumOfDigits = 5
+)
+
+$Num = $NumOfDigits
+
+
+$rma = "RMA-" + -join ([char[]](48..57)*100 | Get-Random -Count $Num)
+$date = get-date -Format d 
+
+$rma + " | " + $date
+
+
+
